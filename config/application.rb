c25db6837963cc13121a81670979b07996060971
Rachel <lastexyle@gmail.com>
2017-04-16 15:02:22 -0700

    Browserify Config

--- config/application.rb
+++ config/application.rb
@@ -63,9 +63,9 @@ module Mastodon
     config.middleware.use Rack::Attack
     config.middleware.use Rack::Deflater
 
-    config.browserify_rails.use_exorcist = true
-    config.browserify_rails.source_map_environments << 'development' << 'production'
-    config.browserify_rails.commandline_options = '--transform [ babelify ] --fast --extension=.jsx --extension=.js '
+    config.browserify_rails.source_map_environments << 'development'
+    config.browserify_rails.commandline_options = '-t babelify --extension=.jsx'
+    config.browserify_rails.evaluate_node_modules = true
 
     config.to_prepare do
       Doorkeeper::AuthorizationsController.layout 'public'
