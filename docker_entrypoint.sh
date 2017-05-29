3a8433522ed4b04a263a617e5c92c01c79252918
Rachel <lastexyle@gmail.com>
2017-05-29 14:10:39 -0700

    Don't care about this security issue

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
 
-### 1. Adds local user (UID and GID are provided from environment variables).
-### 2. Updates permissions, except for ./public/system (should be chown on previous installations).
-### 3. Executes the command as that user.
-
-echo "Creating mastodon user (UID : ${UID} and GID : ${GID})..."
-addgroup -g ${GID} mastodon && adduser -h /mastodon -s /bin/sh -D -G mastodon -u ${UID} mastodon
-
-echo "Executing process..."
-exec su-exec mastodon:mastodon /sbin/tini -- "$@"
+exec /sbin/tini -- "$@"
